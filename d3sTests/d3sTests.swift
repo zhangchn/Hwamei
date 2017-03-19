6c08023cfb26bd48e310ead44bc86a3957f05725
Zhang Chen <tgfbeta@me.com>
2017-03-19 22:26:31 +0800

    add test for data-enter-append

--- d3sTests/d3sTests.swift
+++ d3sTests/d3sTests.swift
@@ -7,12 +7,16 @@
 //
 
 import XCTest
+import UIKit
+import QuartzCore
+
 @testable import d3s
 
 class d3sTests: XCTestCase {
-    
+    var view: UIView!
     override func setUp() {
         super.setUp()
+        view = UIView()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -21,9 +25,19 @@ class d3sTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testDataAppend() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let predicate = NSPredicate(format: "%K = 's'", "age")
+        let sel = view.selectAll(predicate).data(value: [[1, 2], [2, 3, 7]]).enter()
+        XCTAssertTrue(sel._enterGroups.count == 1)
+        let newNodes = sel.append { (node, data, idx, group) -> CALayer? in
+            return CALayer()
+        }
+        XCTAssertTrue(newNodes._groups.count == 1)
+        let groups = newNodes._groups
+        XCTAssertTrue((groups[0][1].value(forKey: "__data__") as? [Int])! == [2, 3, 7])
+        XCTAssertTrue(groups[0][0].superlayer == view.layer)
     }
     
     func testPerformanceExample() {
