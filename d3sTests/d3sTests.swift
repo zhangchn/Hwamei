9e79c0e77f8cce035f0f0784f1502e5886f1e116
Zhang Chen <tgfbeta@me.com>
2017-03-20 10:17:02 +0800

    more test cases

--- d3sTests/d3sTests.swift
+++ d3sTests/d3sTests.swift
@@ -13,7 +13,7 @@ import QuartzCore
 @testable import d3s
 
 class d3sTests: XCTestCase {
-    var view: UIView!
+    var view: UIView?
     override func setUp() {
         super.setUp()
         view = UIView()
@@ -23,13 +23,14 @@ class d3sTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        view = nil
     }
     
     func testDataAppend() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let predicate = NSPredicate(format: "%K = 's'", "age")
-        let sel = view.selectAll(predicate).data(value: [[1, 2], [2, 3, 7]]).enter()
+        let sel = view!.selectAll(predicate).data(value: [[1, 2], [2, 3, 7]]).enter()
         XCTAssertTrue(sel._enterGroups.count == 1)
         let newNodes = sel.append { (node, data, idx, group) -> CALayer? in
             return CALayer()
@@ -37,14 +38,25 @@ class d3sTests: XCTestCase {
         XCTAssertTrue(newNodes._groups.count == 1)
         let groups = newNodes._groups
         XCTAssertTrue((groups[0][1].value(forKey: "__data__") as? [Int])! == [2, 3, 7])
-        XCTAssertTrue(groups[0][0].superlayer == view.layer)
+        XCTAssertTrue(groups[0][0].superlayer == view!.layer)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDataUpdate() {
+        let layer1 = CALayer()
+        layer1.setValue(3, forKey: "cal")
+        view!.layer.addSublayer(layer1)
+        
+        let p = NSPredicate(format: "cal = 3")
+        let update = view!.selectAll(p).data(value: [2, 3])
+        let groups = update._groups
+        XCTAssertTrue(groups[0][0] == layer1)
+        
     }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
