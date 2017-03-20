feff4f067c8f8a23a7c66548774de59b6fc1b305
Zhang Chen <tgfbeta@me.com>
2017-03-20 10:16:44 +0800

    saving workspace and playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,12 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import d3s
 
 var str = "Hello, playground"
 
-let view = UIView()
+let view = UIView.init(frame: CGRect(x: 0, y:0 , width: 30, height: 60))
 
-let sel = Selection(
+view.layer.style = ["cornerRadius": 3.0, "backgroundColor" : #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).cgColor]
+
+view
